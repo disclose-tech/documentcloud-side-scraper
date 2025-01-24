ee6bec61e02bd689a8b54dc6bc0a8022b9e5cc2b
disclose-tech <152188727+disclose-tech@users.noreply.github.com>
2025-01-24 11:30:51 +0100

    Matched year in source_scraper

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -327,7 +327,7 @@ class UploadPipeline:
             "category": item["category"],
             "category_local": item["category_local"],
             "event_data_key": item["event_data_key"],
-            "source_scraper": f"SIDE Scraper {spider.target_years[0]}-{spider.target_years[-1]}",
+            "source_scraper": f"SIDE Scraper {str(item['year'])}",
             "source_file_url": item["source_file_url"],
             "source_filename": item["source_filename"],
             "source_page_url": item["source_page_url"],
